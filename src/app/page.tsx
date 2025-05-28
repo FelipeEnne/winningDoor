c0d35c8a751a3c70fc1068aa2c071c3ac54a9250
FelipeEnne <felipeenne@gmail.com>
2025-05-27 23:13:54 -0300

    add card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,26 @@
 "use client";
 
+import styles from "./Page.module.css";
+import Card from "@/components/Card/card";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div style={{ display: "flex" }}>
-      <h1>Start the game</h1>
+    <div className={styles.form}>
+      <div className="">
+        <Card bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Card>
+        <Card></Card>
+      </div>
+      <div className="">
+        <Card></Card>
+        <Card bgcolor="#29a085">
+          <Link href={`/game/4/2`}>
+            <h2 className={styles.link}>Iniciar</h2>
+          </Link>
+        </Card>
+      </div>
     </div>
   );
 }
